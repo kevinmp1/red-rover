d02b187cb6a05f82fc3d8edf658129d24f7b9be6
stella <stella.lin@cognizant.com>
2020-03-10 13:14:26 -0500

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="app">
+    <Navbar />
+    <div style="padding-top: 50px;">
+      <HelloWorld msg="Welcome to Your Vue.js App"/>
+    </div>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+import Navbar from "./components/Navbar";
+
+export default {
+  name: 'App',
+  components: {
+    Navbar,
+    HelloWorld
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  background-image: linear-gradient(90deg, #000000, #fff);
+  height: 200vh;
+  display: flex;
+}
+</style>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,56 @@
+<template>
+    <div class="bar">
+        <img src="@/assets/rover.png" alt="Red Rover" class="nav-logo"/>
+
+        <div class="options">
+            <ul>
+                <li><a>Curiosity</a></li>
+                <li><a>Spirit</a></li>
+                <li><a>Opportunity</a></li>
+            </ul>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'Navbar',
+        props: {
+        }
+    }
+</script>
+
+<style scoped>
+    .bar {
+        width: 100%;
+        background-color: rgba(52, 6, 10, 0.3);
+        height: 50px;
+        position: fixed;
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        line-height: 50px;
+    }
+
+    .bar div.options {
+        display: flex;
+        justify-content: space-between;
+    }
+
+    .bar div.options ul li {
+        list-style: none;
+    }
+
+    .bar div.options ul {
+        display: flex;
+    }
+
+    .bar div.options ul li a {
+
+    }
+
+    .nav-logo {
+        height: 40px;
+        width: 40px;
+    }
+</style>
