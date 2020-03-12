2def28ae5dfe433edacdf9c801e50fd573d80ec1
stella <stella.lin@cognizant.com>
2020-03-12 08:55:15 -0500

    continued formatting of images

--- src/components/ImagePage.vue
+++ src/components/ImagePage.vue
@@ -2,9 +2,7 @@
     <div class="container">
         <div class="grid">
             <template v-for="(image, key) in images">
-                <div class="cell" :key="key">
-                    <img :src="image"  v-if="image"/>
-                </div>
+                <img :src="image" :key="key" v-if="image"/>
             </template>
         </div>
     </div>
@@ -24,22 +22,43 @@
             rover: String,
             token: String
         },
+        methods: {
+            shuffle: function (array) {
+                let currentIndex = array.length, temporaryValue, randomIndex;
+
+                // While there remain elements to shuffle...
+                while (0 !== currentIndex) {
+
+                    // Pick a remaining element...
+                    randomIndex = Math.floor(Math.random() * currentIndex);
+                    currentIndex -= 1;
+
+                    // And swap it with the current element.
+                    temporaryValue = array[currentIndex];
+                    array[currentIndex] = array[randomIndex];
+                    array[randomIndex] = temporaryValue;
+                }
+                return array;
+            }
+        },
         mounted() {
             axios.get(`https://api.nasa.gov/mars-photos/api/v1/rovers/${this.rover}/photos/?earth_date=2015-6-2&api_key=${this.token}`)
-                .then(response => response.data.photos.forEach( (photo) => {
+                .then(response => {
+                    response.data.photos.forEach( (photo) => {
+                        //remove images of low resolution
+                        let img = new Image();
+                        img.onload = () => {
+                            if (img.height < 300 || img.width < 300) {
+                                let key = this.images.findIndex(url => url === photo.img_src);
+                                this.images.splice(key,1);
+                            }
+                        };
+                        img.src = photo.img_src;
 
-                    //remove images of low resolution
-                    let img = new Image();
-                    img.onload = () => {
-                        if (img.height < 300 || img.width < 300) {
-                            let key = this.images.findIndex(url => url === photo.img_src);
-                            this.images.splice(key,1);
-                        }
-                    };
-                    img.src = photo.img_src;
-
-                    this.images.push(photo.img_src);
-                }))
+                        this.images.push(photo.img_src);
+                    });
+                    this.images = this.shuffle(this.images);
+                });
         }
     }
 </script>
@@ -47,21 +66,19 @@
 <style scoped>
     .container {
         margin: 70px auto 0;
-        max-width: 1200px;
+        /*max-width: 1200px;*/
     }
 
     .container img {
-        max-width: 100%;
+        /*max-width: 100%;*/
+        width: 400px;
+        margin: 0 1rem 1rem 0;
+        display: inline-block;
     }
 
     .grid {
-        display: flex;
-        flex-wrap: wrap;
-    }
-
-    .cell {
-        max-width: 50%;
-        flex-grow: 1;
+        columns: 4 200px;
+        column-gap: 1rem;
     }
 
 </style>
