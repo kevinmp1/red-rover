5396bf109fadaa3610a8ccb57795a9fd062f49f6
Kevin Peterson <kevinmpeterson1@gmail.com>
2020-04-01 13:16:28 -0500

    navbar tests

--- src/components/navbar/Navbar.spec.js
+++ src/components/navbar/Navbar.spec.js
@@ -7,4 +7,16 @@ describe("navbar", () => {
         const wrapper = shallowMount(Navbar, {stubs: ['router-link']});
         expect(wrapper).toBeTruthy();
     });
+
+    it("should route to '/' on click", () => {
+        const mockPush = jest.fn();
+        const $router = {
+            push: mockPush
+        };
+        const wrapper = shallowMount(Navbar, { stubs: ["router-link"], mocks: { $router } });
+      
+        wrapper.find('img.nav-logo').trigger('click');
+
+        expect(mockPush).toHaveBeenCalledWith('/');
+    });
 });
